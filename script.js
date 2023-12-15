d14196f4cdf55d2d810781026f3a00e712b85038
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-15 12:25:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function mapLetters(word) {
+  const letterMap = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+
+    if (!letterMap.hasOwnProperty(letter)) {
+      letterMap[letter] = [i];
+    } else {
+      letterMap[letter].push(i);
+    }
+  }
+
+  return letterMap;
+}
+
+// Take user input using prompt
+const userInput = prompt("Enter a word (all lowercase):");
+
+// Check if the user entered something
+if (userInput !== null && userInput !== "") {
+  // Call the mapLetters function with the user input
+  const result = mapLetters(userInput);
+
+  // Display the result using alert
+  alert(JSON.stringify(result, null, 2));
+} else {
+  // Alert the user if they didn't enter anything
+  alert("You didn't enter a word. Please try again.");
+}
